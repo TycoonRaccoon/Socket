9449d9d26ff833395028493da227c9f887cb1987
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-05-21 15:26:53 -0300

    :recycle: refactors `server`

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,24 +3,37 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import server.game.Game;
+import server.oddOrEven.OddOrEven;
+import util.Logger;
+
 public class Server {
   private int PORT = 8080;
   private ServerSocket serverSocket;
+  private Game game;
 
   public void start() {
     try {
       serverSocket = new ServerSocket(PORT);
+      game = OddOrEven.getInstance();
+      if (game == null)
+        Logger.warn("Game not setted!");
       handleConnections();
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  private void handleConnections() throws IOException {
-    while (true) {
-      System.out.println("Waiting Connection Request.");
-      var clientSocket = serverSocket.accept();
-      System.out.println("Connection Stablished.");
+  private void handleConnections() {
+    try {
+      while (true) {
+        Logger.info("Waiting connection request...");
+        var clientSocket = serverSocket.accept();
+        Logger.info("Connection stablished!");
+        game.addClient(clientSocket);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
