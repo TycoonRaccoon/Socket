806b641e465d9afc337dfb19de8b6d1f4247a11c
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-05-21 15:25:22 -0300

    :recycle: refactors `game`

--- src/server/game/Game.java
+++ src/server/game/Game.java
@@ -3,29 +3,32 @@ package server.game;
 import java.net.Socket;
 
 import lombok.Getter;
+import server.connection.Connection;
+import util.Logger;
 
 public abstract class Game {
-  protected WorkerFactory workerFactory;
+  protected IWorkerFactory workerFactory;
   protected Queue queue;
   @Getter
   protected int playerAmount;
 
-  public Game(int playerAmount, WorkerFactory workerFactory) {
+  public Game(int playerAmount, IWorkerFactory workerFactory) {
     this.playerAmount = playerAmount;
     this.workerFactory = workerFactory;
-    this.queue = new Queue(playerAmount, this::start);
+    this.queue = new Queue(playerAmount, start);
   }
 
-  public void start(Player[] players) {
-    System.out.println("Creating game...");
+  private IQueueHandler start = (Connection[] players) -> {
+    Logger.info("Creating game...");
     var worker = workerFactory.create(players);
-    System.out.println("Game created!");
-    System.out.println("Starting game...");
+    Logger.info("Game created!");
+    Logger.info("Starting game...");
     worker.start();
-  }
+  };
 
   public void addClient(Socket socket) {
-    var player = new Player(socket);
-    queue.add(player);
+    var connection = new Connection(socket);
+    connection.createHeartbeat();
+    queue.add(connection);
   }
 }
