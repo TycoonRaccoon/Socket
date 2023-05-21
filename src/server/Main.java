dd8dbb2ad163ac7ac15dc6e748e79cc07529dadc
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-05-21 15:27:48 -0300

    :recycle: refactors `main` at `server`

--- src/server/Main.java
+++ src/server/Main.java
@@ -5,5 +5,9 @@ public class Main {
     var server = new Server();
 
     server.start();
+
+    // TODO: Create shutdown hook
+    // https://stackoverflow.com/questions/750026/how-to-handle-a-closing-application-event-in-java
+    // Runtime.getRuntime().addShutdownHook(null);
   }
 }
