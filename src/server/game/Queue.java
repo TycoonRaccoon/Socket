56e72cc3d6d89da9a9a78a9625feb1565ef93b8c
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-05-21 15:25:04 -0300

    :recycle: refactors `queue`

--- src/server/game/Queue.java
+++ src/server/game/Queue.java
@@ -1,35 +1,59 @@
 package server.game;
 
+import java.util.stream.Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import server.connection.Connection;
+import util.Logger;
+
 public class Queue {
   private int size;
   private int index = 0;
-  private Player[] players;
-  private QueueHandler handler;
+  private Item[] items;
+  private IQueueHandler handler;
 
-  public Queue(int size, QueueHandler handler) {
+  @Data
+  @AllArgsConstructor
+  private class Item {
+    private Connection connection;
+    private int queueIndex;
+  }
+
+  public Queue(int size, IQueueHandler handler) {
     this.size = size;
-    this.players = new Player[size];
+    this.items = new Item[size];
     this.handler = handler;
   }
 
-  public void add(Player player) {
-    player.setQueueIndex(index);
-    players[index++] = player;
-    System.out.println("Socket added to queue!");
+  public void add(Connection connection) {
+    var item = new Item(connection, index);
+    items[index++] = item;
+    Logger.info(Logger.color.bold().yellow("Queue: ") + "Socket " + connection.getAddress() + " added to queue!");
+    Logger.info(Logger.color.bold().yellow("Queue: ") + "Sockets in queue: " + index);
+
+    connection.setOnDisconnect(() -> remove(item));
+
     if (index == size) {
       index = 0;
-      handler.handle(players);
+      handler.apply(getConnections());
     }
   }
 
-  public void remove(Player player) {
-    var lastPlayer = players[--index];
-    players[index] = null;
+  public void remove(Item item) {
+    var lastItem = items[--index];
+    items[index] = null;
 
-    var removedPlayerindex = player.getQueueIndex();
-    players[removedPlayerindex] = lastPlayer;
-    lastPlayer.setQueueIndex(removedPlayerindex);
+    var removedIndex = item.getQueueIndex();
+    items[removedIndex] = lastItem;
+    lastItem.setQueueIndex(removedIndex);
 
-    System.out.println("Socket removed from queue!");
+    Logger.info(
+        Logger.color.bold().yellow("Queue: ") + "Socket " + item.getConnection().getAddress() + " removed from queue!");
+    Logger.info(Logger.color.bold().yellow("Queue: ") + "Sockets in queue: " + index);
+  }
+
+  private Connection[] getConnections() {
+    return Stream.of(items).map(Item::getConnection).toArray(Connection[]::new);
   }
 }
